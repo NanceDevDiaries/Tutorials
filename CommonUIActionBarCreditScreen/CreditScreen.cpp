4771b45d6c67cf6c4823f56c5255c00db10db5e8
Nancy Duguay <nanceDevDiaries@gmail.com>
2024-03-25 23:18:30 -0700

    Tutorial files for CommonUIActionBar tutorial
    Backing class for a Credits screen that has 3 actions in its bottom bar

---
+++ CommonUIActionBarCreditScreen/CreditScreen.cpp
@@ -0,0 +1,20 @@
+﻿// NanceDevDiaries please credit but use for your own devices.
+
+
+#include "CreditScreen.h"
+
+#include "Input/CommonUIInputTypes.h"
+
+void UCreditScreen::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	BackHandle = RegisterUIActionBinding(FBindUIActionArgs(BackInputActionData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleBackAction)));
+	SlowDownHandle = RegisterUIActionBinding(FBindUIActionArgs(SlowDownInputActionData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleSlowDownAction)));
+	SpeedUpHandle = RegisterUIActionBinding(FBindUIActionArgs(SpeedUpInputActionData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleSpeedUpAction)));
+}
+
+void UCreditScreen::HandleBackAction()
+{
+	DeactivateWidget();
+}
---
+++ CommonUIActionBarCreditScreen/CreditScreen.h
@@ -0,0 +1,38 @@
+﻿// NanceDevDiaries please credit but use for your own devices.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LyraActivatableWidget.h"
+#include "CreditScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, meta = (Category = "UI", DisableNativeTick))
+class LYRAGAME_API UCreditScreen : public ULyraActivatableWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeOnInitialized() override;
+
+	void HandleBackAction();
+	UFUNCTION(BlueprintImplementableEvent)
+	void HandleSlowDownAction();
+	UFUNCTION(BlueprintImplementableEvent)
+	void HandleSpeedUpAction();
+
+	UPROPERTY(EditDefaultsOnly)
+	FDataTableRowHandle BackInputActionData;
+
+	UPROPERTY(EditDefaultsOnly)
+	FDataTableRowHandle SlowDownInputActionData;
+
+	UPROPERTY(EditDefaultsOnly)
+	FDataTableRowHandle SpeedUpInputActionData;
+
+	FUIActionBindingHandle BackHandle;
+	FUIActionBindingHandle SlowDownHandle;
+	FUIActionBindingHandle SpeedUpHandle;
+};
